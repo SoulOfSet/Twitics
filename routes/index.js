e7ce9be7470b84df37ab901061ddbb1532906f1a
Jacob Sabella <jacobsabella@yahoo.com>
2019-01-28 22:14:34 -0600

    Rename to Twitics

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Twitchie' });
+  res.render('index', { title: 'Twitics' });
 });
 
 module.exports = router;
